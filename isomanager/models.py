3dea1c9155655a30d5550f45c07e787a6a90ad17
Raphael Carrier <raphael.carrier@gmail.com>
2021-10-16 08:55:29 -0400

    update models

--- isomanager/models.py
+++ isomanager/models.py
@@ -15,15 +15,16 @@ class LocalCatalog(models.Model):
     class VersionScheme(models.TextChoices):
     	SEMVER = 'SEM', _('SemVer')
         CALVER = 'CAL', _('CalVer')   
-    versionscheme = models.CharField
+    versionscheme = models.CharField(
         max_length=3,
         choices=OsType.choices,
         default=OsType.SemVer,
+        )
 
     version = models.CharField(max_length=255)
     sha256sum = models.CharField(max_length=64)
     class OsType(models.TextChoices):
-        Windows = 'WIN', _('Windows')
+        WINDOWS = 'WIN', _('Windows')
         MACOS = 'MAC', _('macOS')             
         LINUX = 'LNX', _('Linux')
         BSD = 'BSD', _('BSD')
@@ -37,13 +38,18 @@ class LocalCatalog(models.Model):
     ostype = models.CharField(
         max_length=3,
         choices=OsType.choices,
-        default=OsType.Linux,
+        default=OsType.Linux
+        )
+    detachedfromhead = models.BooleanField()
+    
+        
 
 
 # The library of ISO images detected locally (updated by folder scan)
 
 class LocalLibrary(models.Model):
     localname = models.CharField
+    datastore = models.ForeignKey(Datastore)
     sha256sum = models.CharField(max_length=64)
     targeted_by = models.ForeignKey(LibraryTarget, on_delete=models.CASCADE)
 
@@ -56,5 +62,17 @@ class LibraryTarget(models.Model):
     updatefrequency = 
     
 
-
-
+class Datastore(models.Model):
+    class DatastoreType(models.TextChoices):
+        PATH =
+        NFS = 
+        CIFS = 
+        SFTP =
+        FTP =
+    datastoretype = models.Charfield(
+        max_length=4,
+        choices = DatastoreType.choices,
+        default = DatastoreType.Localpath,
+    readonly = models.BooleanField()
+	last_update = models.DateTimeField('last update')
+	

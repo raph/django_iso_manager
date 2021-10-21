56180cbc1096e801b5d489189cc68e6784bd1121
raph <raphael.carrier@gmail.com>
2021-10-20 20:26:37 -0400

    Update models.py

--- isomanager/models.py
+++ isomanager/models.py
@@ -54,12 +54,18 @@ class RemoteCatalog(models.Model):
 class OsEdition(models.Model):
     os_edition_name = models.CharField(_('OS Edition'), help_text=_('The edition of the OS'), max_length=32,
                                        unique=True)
+    os_edition_version = models.CharField(_('Version Number'), help_text=_('The version number of the item'), max_length=32)
+    os_edition_arch = models.CharField(_('OS Architecture'), help_text=_('The architecture of the OS'), max_length=16,
+                               choices=OsArch.choices, default=OsArch.AMD64)
+    os_edition_language = models.CharField(_('Language'), help_text=_('The language of the item in the catalog'),
+                                   choices=OsLanguage.choices)
     def __str__(self):
-        return "{0}".format(self.os_edition_name)
+        return "{0} - {1}".format(self.os_edition_name, self.os_edition_version, self.os_edition_arch, self.os_edition_language)
 
     class Meta:
-        verbose_name = _('OS Edition Name')
-        verbose_name_plural = _('OS Edition Names')
+        verbose_name = _('OS Edition')
+        verbose_name_plural = _('OS Edition')
+        unique_together = ("os_edition_name", "os_edition_version", "os_edition_arch", "os_edition_language")
 
 
 class LocalCatalog(models.Model):
@@ -71,17 +77,15 @@ class LocalCatalog(models.Model):
     last_update = models.DateTimeField(_('Last Update'), help_text=_('Time last scanned'))
     version_scheme = models.CharField(_('Version Scheme'), help_text=_('The version scheme being used'), max_length=3,
                                       choices=VersionScheme.choices, default=VersionScheme.SEMVER)
-    version_number = models.CharField(_('Version Number'), help_text=_('The version number of the item'), max_length=255)
+    version_number = models.ForeignKey(OsEdition.os_edition_version, _('Version Number'), help_text=_('The version number of the item'))
     maintainer = models.CharField(_('Maintainer'), help_text=_('The version number of the item'), max_length=255)
     sha256sum = models.CharField(_('SHA256 Checksum'), help_text=_('The SHA256 checksum of the item'), max_length=64,
                                  unique=True)
     os_type = models.CharField(_('OS Type'), help_text=_('The type of image'), max_length=16,
                                choices=OsType.choices, default=OsType.LINUX)
-    os_edition = models.ForeignKey(OsEdition, _('OS Edition'), help_text=_('The edition of the OS'), max_length=32)
-    os_language = models.CharField(_('Language'), help_text=_('The language of the item in the catalog'),
-                                   choices=OsLanguage.choices)
-    os_arch = models.CharField(_('OS Architecture'), help_text=_('The architecture of the OS'), max_length=16,
-                               choices=OsArch.choices, default=OsArch.AMD64)
+    os_edition = models.ForeignKey(OsEdition.os_edition_name, _('OS Edition'), help_text=_('The edition of the OS'), max_length=32)
+    os_language = models.ForeignKey(OsEdition.os_edition_language, _('Language'), help_text=_('The language of the item in the catalog'))
+    os_arch = models.ForeignKey(OsEdition.os_edition_arch, _('OS Architecture'), help_text=_('The architecture of the OS'))
     detached_from_head = models.BooleanField(_('Detached from head'),
                                              help_text=_('True if the item has been manually edited'))
     release_date = models.DateTimeField(_('Release date'), help_text=_('The release date of this version'))
@@ -99,11 +103,21 @@ class LibraryTarget(models.Model):
     """
     The list of items to be downloaded and updated
     """
-    name = models.CharField(max_length=255)
-    desired_version = models.CharField(max_length=32)
-    update_check_frequency = recurrence.fields.RecurrenceField()
+    target_name = models.CharField(_('Auto-Update Name'), help_text=_('The name for this auto-update target'), max_length=64)
+    desired_version = models.CharField(_('Desired Version'), help_text=_('The desired target version of this image'), max_length=32)
+    update_check_frequency = recurrence.fields.RecurrenceField(_('Update frequency'), help_text=_('How often should this image be updated'))
     catalog_item = models.ForeignKey(_('Catalog Item'), help_text=_('The catalog item linked to this target'),
-                                     LocalCatalog, on_delete=models.RESTRICT)
+                                     LocalCatalog, unique=True, on_delete=models.RESTRICT)
+    managed_item = models.ForeignKey(_('Managed Item'), help_text=_('The items currently satisfying this target'), ManagedItem,
+                                    on_delete=models.DO_NOTHING)
+    
+    def __str__(self):
+        return "{0}".format(self.target_name)
+
+    class Meta:
+        verbose_name = _('Auto-Update Target')
+        verbose_name_plural = _('Auto-Update Targets')
+
 
 class ManagedItem(models.Model):
     """"
@@ -113,6 +127,5 @@ class ManagedItem(models.Model):
     full_path = models.CharField(max_length=255)
     datastore = models.ForeignKey(Datastore, on_delete=models.CASCADE)
     sha256sum = models.CharField(max_length=64)
-    targeted_by = models.ForeignKey(_('Targeted by'), help_text=_('The target '), LibraryTarget,
-                                    on_delete=models.DO_NOTHING)
+
 

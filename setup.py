0e5db8f874c3fdfe173d758e5fcc4fa0fcd836e1
Daniel Gagnon <moi@danielgagnon.info>
2021-10-21 00:34:28 -0400

    Add requirements file and setup.py

---
+++ setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+
+setup(name='django_iso_manager', version='1.0.0', packages=['isomanager', 'isomanager.management', 'isomanager.management.commands', 'isomanager.migrations'], url='https://github.com/raph/django_iso_manager', license='', author='dgagnon',
+      author_email='raph@raph.info', description='')

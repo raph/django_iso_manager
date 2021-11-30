3cc497c028144c61ca5014c59f7af642cd2b3431
ketulsuthar <k2lsuthar@gmail.com>
2021-11-29 20:19:14 -0500

    updated code for loding default entru for remote catalog

--- isomanager/apps.py
+++ isomanager/apps.py
@@ -6,4 +6,15 @@ class IsomanagerConfig(AppConfig):
     name = 'isomanager'
 
     def ready(self):
-        from . import signals  # noqa
+        remote_catalog = self.get_model('RemoteCatalog')
+
+        if remote_catalog.objects.count() < 1:
+            payload = {
+                "catalog_name":"default",
+                "json_catalog":None,
+                "version":"1.0",
+                "auto_update":True,
+                "priority":"1",
+                "remote_url":"http://raph.info/dim.json"
+            }
+            remote_catalog.objects.create(**payload)
